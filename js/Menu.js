156ba2d8f2334c0e7dd409842e4f23ca3873d11a
Juha <haapajarvi@gmail.com>
2016-03-27 18:38:50 +0300

    munu.js add

---
+++ js/Menu.js
@@ -0,0 +1,8 @@
+  function SetMenuSelected(e) {
+    // sets all menuitems not selected and menuItem selected
+    var table = document.getElementById('selSpace'),
+    selected = table.getElementsByClassName('menuitemSelected');
+    
+    if (selected[0]) selected[0].className = 'menuitem';
+    e.target.parentNode.className = 'menuitemSelected;
+  }
