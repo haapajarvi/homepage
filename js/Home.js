f0d113fb99fe441fb91055d46103fbe21af39a4a
Juha <haapajarvi@gmail.com>
2016-03-28 03:10:27 +0300

    Html include versio 2

---
+++ js/Home.js
@@ -0,0 +1,10 @@
+/**
+ * @author haba
+ */
+
+function ContactInformation(){
+           // document.getElementById("textDiv").innerHTML='<object type="type/html" data="Juha.htm" ></object>';
+           
+           jQuery(function($){$('#textDiv').load('Juha.htm');
+});
+  }
